f01690c0127df78e1c675ca5d286c9ed1385d7f4
AlessandroSouzaSilva <alessandroanalistati@gmail.com>
2022-08-04 03:36:22 -0300

    perguntas ok

--- chatbot/capturas/views.py
+++ chatbot/capturas/views.py
@@ -3,7 +3,7 @@ from .models import Captura
 
 # Create your views here.
 def capturas(request, code_user):
-	titulo = 'Capturas de Informação'
+	titulo = 'CAPTURA DE DADOS DO USUÁRIOS'
 	captura = Captura.objects.filter(code_user=code_user)
 	return render(request, 'capturas.html', 
 				 {'titulo': titulo, 'capturas': captura, 'code_user': code_user})
